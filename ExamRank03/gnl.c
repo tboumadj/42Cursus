c8b66c5e36b599096dc7c81e41dd7c42c77b65e8
tboumadj <93995176+tboumadj@users.noreply.github.com>
2022-11-07 19:10:03 +0100

    Add files via upload

---
+++ ExamRank03/gnl.c
@@ -0,0 +1,16 @@
+/*    main  uni <std + stdlib>
+
+static STR + char TMP
+return TMP
+SAVE apres \n dans STR
+
+Read le tout et save dans STR
+
+Save de str a TMP toiut ce kil y a juska \n
+
+separer avant \n et garder le reste dans STR */
+
+char    *get_next_line(int fd)
+{
+    
+}
